a41f0b720efab07bf8c0c0e7270a68a83826e475
Jaehyeong Kwon <89461624+Stillav@users.noreply.github.com>
2023-09-04 14:14:56 +0900

    [level 1] Title: 삼총사, Time: 0.06 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/131705. 삼총사/삼총사.py
@@ -0,0 +1,9 @@
+def solution(number):
+    import itertools 
+    answer = 0
+    combs = list(itertools.combinations(number, 3))
+    
+    for comb in combs:
+        if sum(comb) == 0:
+            answer += 1
+    return answer
