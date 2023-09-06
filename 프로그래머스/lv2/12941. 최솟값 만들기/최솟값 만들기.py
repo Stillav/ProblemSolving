bee088c7a150bb494fb38decda5c21eed7684b22
Jaehyeong Kwon <89461624+Stillav@users.noreply.github.com>
2023-09-06 22:03:34 +0900

    [level 2] Title: 최솟값 만들기, Time: 0.33 ms, Memory: 9.94 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12941. 최솟값 만들기/최솟값 만들기.py
@@ -0,0 +1,11 @@
+def solution(A,B):
+    answer = 0
+
+    # [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+    A.sort()
+    B.sort(reverse=True)
+    
+    for i, j in zip(A,B):
+        answer += i * j
+
+    return answer
