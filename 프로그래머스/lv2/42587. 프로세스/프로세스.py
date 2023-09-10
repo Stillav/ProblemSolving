7dafa76c45e34da25d4b5939ec88291e425e2d90
Jaehyeong Kwon <89461624+Stillav@users.noreply.github.com>
2023-09-10 11:56:01 +0900

    [level 2] Title: 프로세스, Time: 0.12 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/42587. 프로세스/프로세스.py
+++ 프로그래머스/lv2/42587. 프로세스/프로세스.py
@@ -1,19 +1,15 @@
 def solution(priorities, location):
     answer = 0
-    alphas = [chr(order) for order in range(ord('A'), ord('A') + len(priorities))]
+    queue = [(i, p) for i, p in enumerate(priorities)]
     
-    alphas_pair = [[key, value] for key, value in zip(alphas, priorities)]
-    complete = []
-    i = 1 
-    while len(alphas_pair) != 0:
-        max_priority = max(priorities)
-        i += 1 
-        if alphas_pair[0][1] != max_priority:
-            alphas_pair.append(alphas_pair.pop(0))
+    while True:
+        cur = queue.pop(0)
+        
+        if any(cur[1] < q[1] for q in queue):
+            queue.append(cur)
         else:
-            complete_value = alphas_pair.pop(0)
-            complete.append(complete_value[0])
-            priorities.remove(complete_value[1])
-            
-    answer = (complete.index(chr(ord('A') + location)) + 1)
+            answer += 1 
+            if location == cur[0]:
+                return answer 
+        
     return answer
