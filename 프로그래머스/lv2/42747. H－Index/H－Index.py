30c3f0421dbed5e18cf3865a2e585eaeb0a94819
Jaehyeong Kwon <89461624+Stillav@users.noreply.github.com>
2023-09-12 14:50:40 +0900

    [level 2] Title: H-Index, Time: 0.01 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/lv2/42747. H－Index/H－Index.py
+++ 프로그래머스/lv2/42747. H－Index/H－Index.py
@@ -3,7 +3,9 @@ def solution(citations):
     
     citations.sort(reverse=True)
     
-    for i, citation in enumerate(citations):
-        if (i+1) <= citation:
-            answer = (i+1)
+    for i, citation in enumerate(citations, start=1):
+        if i <= citation:
+            answer = i
+            
+    
     return answer
