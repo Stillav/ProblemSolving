7a614b6c54ed515d151fa8939e17a4d4d7178a83
Jaehyeong Kwon <89461624+Stillav@users.noreply.github.com>
2023-09-15 19:50:17 +0900

    [level 2] Title: 더 맵게, Time: 1747.61 ms, Memory: 51.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42626. 더 맵게/더 맵게.py
@@ -0,0 +1,21 @@
+def solution(scoville, K):
+    import heapq
+    
+    heapq.heapify(scoville)
+    answer = 0
+    now = scoville[0]
+
+    while now < K and len(scoville) > 1:
+
+        first = heapq.heappop(scoville)
+        second = heapq.heappop(scoville)
+
+        new = first + second*2
+        answer += 1
+        heapq.heappush(scoville, new)
+        now = scoville[0]
+
+    if min(scoville) < K:
+        answer = -1
+
+    return answer
